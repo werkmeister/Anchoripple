3563fdf9f6f705238c4b3e669f2f3983db132f5b
Joshua Werkmeister <joshuawerkmeister@joshuas-mbp.attlocal.net>
2015-04-28 20:36:22 -0500

    more content

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,47 @@
-/* jshint devel:true */
-console.log('\'Allo \'Allo!');
+'use strict';
+
+function navResize( offset, flip ) {
+  var height = 200 - (offset);
+  var imgWidth = 264 - (offset * 1.75);
+  var margin = -132 + (offset * 0.875);
+  var top = 80 - (offset * 0.4);
+
+    $('.header').css({
+      'height': height + 'px',
+    });
+    $('.header ul').css('top', top + 'px');
+    $('.header-img').css({
+      'margin-left': margin,
+      'width': imgWidth
+    });
+
+    $('.header-img').toggleClass('rotate', flip);
+}
+
+function headerScroll() {
+  var offset = window.pageYOffset;
+
+  if (offset < 135) {
+    navResize( offset, false );
+  } else {
+    offset = 134;
+    navResize( offset, true );
+  }
+}
+
+
+function navigate( e ) {
+  // e.stopPropagation();
+  // e.preventDefault();
+  $('section').addClass('hidden');
+
+  $('section.' + this.dataset.navigate).toggleClass('hidden');
+}
+
+$('a').on('click', navigate);
+
+window.onload = headerScroll();
+
+$(window).on('scroll', function() {
+  headerScroll();
+});
