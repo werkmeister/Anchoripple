38d6f053f8485c26648149e8ffeb65f57e91e3ff
Joshua Werkmeister <jwerkme@gmail.com>
2015-06-08 17:28:16 -0500

    new contact and some new images

--- app/contact.php
+++ app/contact.php
@@ -1,56 +1,52 @@
-
 <?php
-	require_once './PHPMailer-master/PHPMailerAutoload.php';
+    // My modifications to mailer script from:
+    // http://blog.teamtreehouse.com/create-ajax-contact-form
+    // Added input sanitizing to prevent injection
 
-	if($_SERVER["REQUEST_METHOD"] == "POST"){
-		$name = trim($_POST["name"]);
-		$email = trim($_POST["email"]);
-		$organization = trim($_POST["organization"]);
-		$message = trim($_POST["message"]);
+    // Only process POST reqeusts.
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Get the form fields and remove whitespace.
+        $name = strip_tags(trim($_POST["name"]));
+				$name = str_replace(array("\r","\n"),array(" "," "),$name);
+        $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $message = trim($_POST["message"]);
 
-		if($name == "" || $email == "" || $message == ""){
-	       	$error_message = "All fields requred";
-		}
+        // Check that data was sent to the mailer.
+        if ( empty($name) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // Set a 400 (bad request) response code and exit.
+            http_response_code(400);
+            echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+            exit;
+        }
 
-		if(!isset($error_message)){
-			foreach($_POST as $value){
-				if(stripos($value, 'Content-Type:') != FALSE){
-					$error_message = "There was a problem";
-					}
-				}
-		}
-		if(!isset($error_message) && $_POST["something"] != ""){
-			$error_message = "Sorry that field should not be filled out";
-		}
+        $recipient = "stephenmlamb@gmail.com";
 
-		$mail = new PHPMailer(); // defaults to using php "mail()"
+        // Set the email subject.
+        $subject = "New contact from $name";
 
-		if (!isset($error_message) && !$mail->ValidateAddress($email)){
-			$error_message = "You must specify a valid email address<br/>";
-		}
-		//set email body with Post information
-		if(! isset($error_message)){
-			$body = "";
-			$body = $body . "Name: " . $name . "<br/>";
-			$body = $body . "Email: " . $email . "<br/>";
-			$body = $body . "Message: " . $message . "<br/>";
+        // Build the email content.
+        $email_content = "Name: $name\n";
+        $email_content .= "Email: $email\n\n";
+        $email_content .= "Message:\n$message\n";
 
-			//populate email to stevenmlamb@gmail.com
-			$mail->SetFrom($email, $name);
-			// $address = "jwerkme@gmail.com";
-			$address = "stevenmlamb@gmail.com";
-			$mail->AddAddress($address, "Anchoripple Contact");
-			$mail->Subject    = "Anchoripple Message From: " . $name;
-			$mail->MsgHTML($body);
+        // Build the email headers.
+        $email_headers = "From: $name <$email>";
 
-			if($mail->Send()) {
-				// TODO: figure out how to use header in this case
-				// header("Location: playground.php?status=thanks");
-				echo 'Thank You ' . $name;
-				exit;
-			}else{
-				$error_message = "Mailer Error: " . $mail->ErrorInfo;
-			}
-		}
-	}
+        // Send the email.
+        if (mail($recipient, $subject, $email_content, $email_headers)) {
+            // Set a 200 (okay) response code.
+            http_response_code(200);
+            echo "Thank You! Your message has been sent.";
+        } else {
+            // Set a 500 (internal server error) response code.
+            http_response_code(500);
+            echo "Oops! Something went wrong and we couldn't send your message.";
+        }
+
+    } else {
+        // Not a POST request, set a 403 (forbidden) response code.
+        http_response_code(403);
+        echo "There was a problem with your submission, please try again.";
+    }
+
 ?>
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -41,21 +41,71 @@ $(window).on('scroll', function() {
   headerScroll();
 });
 
-$('form').submit(function( e ) {
+// $('form').submit(function( e ) {
+//   e.preventDefault();
+//   e.stopPropagation();
+//
+// // TODO: make this awesome
+//   var email = {
+//     'name': $('input[name=name]').val(),
+//     'email': $('input[name=email]').val(),
+//     'organization': $('input[name=organization]').val(),
+//     'message': $('textarea[name=message]').val()
+//   };
+//
+//   $.post('contact.php', email, function( response ) {
+//     console.log(response);
+//   });
+// });
+/***********
+SEND EMAILS
+***********/
+// Get the form.
+var form = $('#ajax-contact');
+
+// Get the messages div.
+var formMessages = $('#form-messages');
+
+// Set up an event listener for the contact form.
+$(form).submit(function(e) {
+  // Stop the browser from submitting the form.
   e.preventDefault();
-  e.stopPropagation();
 
-// TODO: make this awesome
-  var email = {
-    'name': $('input[name=name]').val(),
-    'email': $('input[name=email]').val(),
-    'organization': $('input[name=organization]').val(),
-    'message': $('textarea[name=message]').val()
-  };
+  // Serialize the form data.
+  var formData = $(form).serialize();
 
-  $.post('contact.php', email, function( response ) {
-    console.log(response);
+  // Submit the form using AJAX.
+  $.ajax({
+    type: 'POST',
+    url: $(form).attr('action'),
+    data: formData
+  })
+  .done(function(response) {
+    // Make sure that the formMessages div has the 'success' class.
+    $(formMessages).removeClass('error');
+    $(formMessages).addClass('success');
+
+    // Set the message text.
+    $(formMessages).text(response);
+
+    // Clear the form.
+    $('#name').val('');
+    $('#email').val('');
+    $('#message').val('');
+  })
+  .fail(function(data) {
+    // Make sure that the formMessages div has the 'error' class.
+    $(formMessages).removeClass('success');
+    $(formMessages).addClass('error');
+
+    // Set the message text.
+    if (data.responseText !== '') {
+      $(formMessages).text(data.responseText);
+    } else {
+      $(formMessages).text('Oops! An error occured and your message could not be sent.');
+    }
   });
+
 });
 /************
 // FOLD PANEL : AUTHOR Sebastiano Guerriero http://codyhouse.co/gem/3d-folding-panel/
